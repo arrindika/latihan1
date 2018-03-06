e001a2c5cd0a13e85fdf2e63d97ece65120be5a5
arrindika <arrindika.pradana@gmail.com>
2018-03-07 05:27:24 +0700

    ubah 1

--- mailer.php
+++ mailer.php
@@ -1,56 +1,22 @@
 <?php
-    // My modifications to mailer script from:
-    // http://blog.teamtreehouse.com/create-ajax-contact-form
-    // Added input sanitizing to prevent injection
+$name=$_POST['name'];
+$email=$_POST['email'];
+$subject=$_POST['subject'];
+$message=$_POST['message'];
 
-    // Only process POST reqeusts.
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Get the form fields and remove whitespace.
-        $name = strip_tags(trim($_POST["name"]));
-				$name = str_replace(array("\r","\n"),array(" "," "),$name);
-        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-        $cont_subject = trim($_POST["subject"]);
-        $message = trim($_POST["message"]);
+$to='gullacindy048@gmail.com';
 
-        // Check that data was sent to the mailer.
-        if ( empty($name) OR empty($message) OR empty($cont_subject) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            // Set a 400 (bad request) response code and exit.
-            http_response_code(400);
-            echo "Oops! There was a problem with your submission. Please complete the form and try again.";
-            exit;
-        }
+$message="From:$name <br />To: Admin@it-its.com <br />Pengaduan: ".$message;
 
-        // Set the recipient email address.
-        // FIXME: Update this to your desired email address.
-        $recipient = "csealtaf@gmail.com";
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
 
-        // Set the email subject.
-        $subject = "New contact from $name";
-
-        // Build the email content.
-        $email_content = "Name: $name\n";
-        $email_content .= "Email: $email\n\n";
-        $email_content .= "Subject: $cont_subject\n";
-        $email_content .= "Message:\n$message\n";
-
-        // Build the email headers.
-        $email_headers = "From: $name <$email>";
-
-        // Send the email.
-        if (mail($recipient, $subject, $email_content, $email_headers)) {
-            // Set a 200 (okay) response code.
-            http_response_code(200);
-            echo "Thank You! Your message has been sent.";
-        } else {
-            // Set a 500 (internal server error) response code.
-            http_response_code(500);
-            echo "Oops! Something went wrong and we couldn't send your message.";
-        }
-
-    } else {
-        // Not a POST request, set a 403 (forbidden) response code.
-        http_response_code(403);
-        echo "There was a problem with your submission, please try again.";
-    }
-
+// More headers
+$headers .= 'From: '.$email . "\r\n";
+$headers .= 'Cc: '.$email . "\r\n";
+@mail($to,$subject,$message,$headers);
+if(@mail)
+{
+echo "Email sent successfully !!";	
+}
 ?>
